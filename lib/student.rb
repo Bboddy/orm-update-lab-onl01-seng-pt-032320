e03940b7f3f170f72796440bf1933e9e4fb37740
Bryce B <bryce.boddy@gmail.com>
2020-06-10 08:22:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,16 @@ class Student
     student = Student.new(array[1], array[2], array[0])
   end
   
-  def self.find_by_name
-    
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 end
